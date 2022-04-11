4548d769239ab2268c761cd97c71ec10e7cdfe1a
Elvie Rayl <imaf6971@gmail.com>
2022-04-11 21:35:55 +0300

    initial commit

---
+++ src/test/java/ru/tisbi/college/CollegeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tisbi.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
