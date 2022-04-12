4c162d88f5f9d7ae2381f4974b92c863ebf3af42
Vlad Mironov <imaf6971@gmail.com>
2022-04-12 19:22:40 +0300

    implemented add group form

--- src/main/java/ru/tisbi/college/groups/GroupsController.java
+++ src/main/java/ru/tisbi/college/groups/GroupsController.java
@@ -1,24 +1,48 @@
 package ru.tisbi.college.groups;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ru.tisbi.college.specializations.SpecializationService;
 
 @Controller
-@RequestMapping("/groups")
 public class GroupsController {
 
     private final GroupService groupService;
 
-    public GroupsController(GroupService groupService) {
+    private final SpecializationService specializationService;
+
+    public GroupsController(GroupService groupService, SpecializationService specializationService) {
         this.groupService = groupService;
+        this.specializationService = specializationService;
     }
 
-    @GetMapping
+    @GetMapping("/groups")
     public String getGroupList(Model model) {
         model.addAttribute("specializations", groupService.groupsBySpecialization());
         return "groups";
     }
 
+    @GetMapping("/groups/add")
+    public String getAddGroupsPage(Model model) {
+        var specializations = specializationService.findAllSpecializations();
+        model.addAttribute("specializations", specializations);
+        model.addAttribute("group", new GroupDto());
+        return "group-add";
+    }
+
+    @PostMapping("/groups/add")
+    public String newGroup(@ModelAttribute @Valid GroupDto group, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "group-add";
+        }
+        groupService.newGroup(group);
+        return "redirect:/groups";
+    }
 }
