895945efb93f547249765288368b2126e23641b7
Elvie Rayl <imaf6971@gmail.com>
2022-04-14 21:56:28 +0300

    added tests for Group

--- src/test/java/ru/tisbi/college/groups/GroupTest.java
+++ src/test/java/ru/tisbi/college/groups/GroupTest.java
@@ -1,31 +1,51 @@
 package ru.tisbi.college.groups;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Year;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import ru.tisbi.college.students.Group;
 
 public class GroupTest {
     @Test
-    void testToNumberString() {
+    void testGetNumberString() {
+        getNumberStringCaseA();
+        getNumberStringCaseB();
+        getNumberStringCaseC();
+        getNumberStringCaseD();
+    }
+
+    void getNumberStringCaseA() {
+        var underTest = new Group();
+        underTest.setAdmissionYear(Year.of(2019));
+        underTest.setAfterSeniorSchool(false);
+        underTest.setNumber((short) 1);
+        assertThat(underTest.getNumberString()).isEqualTo("191.1");
+    }
+
+    void getNumberStringCaseB() {
         var underTest = new Group();
         underTest.setAdmissionYear(Year.of(2019));
         underTest.setAfterSeniorSchool(false);
         underTest.setNumber((short) 2);
-        assertEquals("191.2", underTest.toNumberString());
+        assertThat(underTest.getNumberString()).isEqualTo("191.2");
     }
 
-    @Test
-    void testStringCompare() {
-        var a = "1.1";
-        var b = "1.2";
+    void getNumberStringCaseC() {
+        var underTest = new Group();
+        underTest.setAdmissionYear(Year.of(2019));
+        underTest.setAfterSeniorSchool(true);
+        underTest.setNumber((short) 1);
+        assertThat(underTest.getNumberString()).isEqualTo("192.1");
+    }
 
-        var result = a.compareTo(b);
-
-        Assertions.assertThat(result).isLessThan(0);
+    void getNumberStringCaseD() {
+        var underTest = new Group();
+        underTest.setAdmissionYear(Year.of(2019));
+        underTest.setAfterSeniorSchool(true);
+        underTest.setNumber((short) 2);
+        assertThat(underTest.getNumberString()).isEqualTo("192.2");
     }
 }
